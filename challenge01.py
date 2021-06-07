8394f547ef61de199fc53e560bfc0f99b08c9d4d
Mark Manipula <markmikomanipula@gmail.com>
2021-06-07 21:08:59 +0000

    manipulating lists

---
+++ challenge01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+name = input("What's your name?\n>")
+dayOfWeek = input("What day is it today?\n>")
+
+print("Hello", name, "!", "Happy", dayOfWeek)
+
+#format method
+print(f"Hello, {name}!  Happy {dayOfWeek}!")
+
