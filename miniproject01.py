e422ade6327dd8fdb30fcdef4cda58d0e19a64a9
Mark Manipula <markmikomanipula@gmail.com>
2021-06-08 22:56:01 +0000

    finished initial game

--- miniproject01.py
+++ miniproject01.py
@@ -26,25 +26,75 @@ print(f"{firstNumber} {secondNumber} {thirdNumber}")
 keepPlaying = True
 A = 0;
 B = 0;
-X ="X"
+X = "No hit!"
+invalidInput = True
+numberOfTries = 0
 
 while keepPlaying:
 
-    try:
-        playerGuessFirstNumber = int(input("Enter first number\n>"))
-    except:
-        print("Enter a valid first number")
+    #validates if user have a non number input
+    while invalidInput:
+        try:
+            playerGuessFirstNumber = int(input("Enter first number\n>"))
+            #based on the rulesm first nubmer cant start at 0
+            #this will prevent the user to input 0
+            if playerGuessFirstNumber == 0:
+                print("First number can't start at 0!")
+                continue
+            break
+        except:
+            print("Enter a valid first number")
 
-    try:
-        playerGuessSecondNumber = int(input("Enter second number\n>"))
-    except:
-        print("Enter a valid second number")
+    while invalidInput:
+        try:
+            playerGuessSecondNumber = int(input("Enter second number\n>"))
+            if playerGuessFirstNumber == playerGuessSecondNumber:
+                print("You cant have repeating numbers!")
+                continue
+            break
+        except:
+            print("Enter a valid second number")
 
-    try:    
-        playerGuessThirdNumber = int(input("Enter third number\n>"))
-    except:
-        print("Enter a valid third number")
+    while invalidInput:
+        try:
+            playerGuessThirdNumber = int(input("Enter third number\n>"))
+            if playerGuessThirdNumber == playerGuessSecondNumber or playerGuessThirdNumber == playerGuessFirstNumber:
+                print("You cant have repeating numbers!")
+                continue
+            invalidInput = False
+            break
+        except:
+            print("Enter a valid third number")
 
+    numberOfTries += 1
+    #goal of the game is to get all three numbers in the right order
+    if firstNumber == playerGuessFirstNumber:
+        A+=1
+    if secondNumber == playerGuessSecondNumber:
+        A+=1
+    if(thirdNumber == playerGuessThirdNumber):
+        A+=1
+    if A == 3:
+        keepPlaying = False
+        print(f"Congratulations, you cracked the 3-digit code!\n>The numbers are {firstNumber} {secondNumber} {thirdNumber} \n>Number of tries: {numberOfTries}")    
+        break
+
+    if(firstNumber == playerGuessSecondNumber or firstNumber == playerGuessThirdNumber):
+        B+=1
+    if(secondNumber == playerGuessFirstNumber or secondNumber == playerGuessThirdNumber):
+        B+=1
+    if(thirdNumber == playerGuessFirstNumber or thirdNumber == playerGuessSecondNumber):
+        B+=1
+
+    if(A == 0 and B == 0):
+        print(X)
+    else:
+        print(f"Your input is {playerGuessFirstNumber}{playerGuessSecondNumber}{playerGuessThirdNumber} --> {A}-A {B}-B")
+
+    #reset the value of a and b to keep the hints accurate
+    invalidInput = True
+    A = 0
+    B = 0
 
 
 
